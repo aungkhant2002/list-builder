9f4ededdb93ed75ed3ec5f3277d536797a20f971
aungkhant2002 <aung.khant@ucsy.edu.mm>
2024-10-11 11:44:17 +0630

    prepare UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      mono: ["Cutive Mono", "monospace"],
+      serif: ["Bree Serif", "serif"],
+    },
     extend: {},
   },
   plugins: [],
